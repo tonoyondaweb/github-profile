70592671082a4e1c02e2fd05205d801f30d09aa4
tanay <iamtanaylodh@gmail.com>
2022-12-04 09:50:56 +0530

    added github logo on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import styles from "../styles/Home.module.css";
 import { UserContextInterface } from "./_app";
 import { userContext } from "./_app";
 import { useRouter } from "next/router";
+import { FiGithub } from "react-icons/fi";
 
 export default function Home() {
 	const input = useContext(userContext) as UserContextInterface;
 	const router = useRouter();
 	return (
 		<div className={styles.home}>
+			<FiGithub className={styles.gitLogo} />
 			<h1 className={styles.prompt}>Enter your username</h1>
 			<input
 				className={styles.input}
